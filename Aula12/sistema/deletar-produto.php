0a678d718dd46f91b9e817aee7aa3984528ba99e
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2025-06-06 21:58:19 -0300

    Add files via upload

---
+++ Aula12/sistema/deletar-produto.php
@@ -0,0 +1,35 @@
+<?php
+    include("conexao.php");
+    $id = $_GET["id"];
+    $sql = "DELETE FROM `estoque` WHERE id_estoque = '$id'";
+    $delete = mysqli_query($conexao, $sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Estilo da Página -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Lista de Produto :: CRUD</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h3>Produto Excluido com Sucesso</h3>
+
+        <div>
+            <a href="listar-produto.php" role="button" class="btn btn-primary btn-sm">Voltar para a lista de produtos</a>
+        </div>
+    </div><!-- container -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
